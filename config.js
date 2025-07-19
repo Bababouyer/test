1ad26007a0f590730a1eeb9416a8f3d6ff5e43c8
Bababouyer <fwig010@gmail.com>
2025-07-19 15:45:56 +0200

    Add bogus AWS, SendGrid, and SMTP secrets for testing

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const sendgrid = "SG.abcd1234fakeapikey5678.abcdefghij0987654321fakeapisecret";
+const smtp = {
+  host: "smtp.fakeserver.com",
+  user: "smtpuser@fakeserver.com",
+  pass: "SuperSecretPassword!"
+};
